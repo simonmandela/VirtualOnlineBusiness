d1124b5f8d682e632462e6e10bf14dcc87274e08
Aamirsohail30350727 <Aamirsohail5>
2021-06-01 11:20:17 +0300

    first initialized the project

---
+++ src/main/java/com/virtualonlinebusiness/VirtualOnlineBusinessApplication.java
@@ -0,0 +1,13 @@
+package com.virtualonlinebusiness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualOnlineBusinessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualOnlineBusinessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/virtualonlinebusiness/VirtualOnlineBusinessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.virtualonlinebusiness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualOnlineBusinessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
